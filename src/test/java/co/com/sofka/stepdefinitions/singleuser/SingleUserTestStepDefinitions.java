b640ae86ebb654c93339f532b6b848f9bada879a
Jose Vicente <velasco.josevicente@gmail.com>
2022-03-24 18:42:19 -0500

    Se deja listo plantilla del proyecto

---
+++ src/test/java/co/com/sofka/stepdefinitions/singleuser/SingleUserTestStepDefinitions.java
@@ -0,0 +1,24 @@
+package co.com.sofka.stepdefinitions.singleuser;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SingleUserTestStepDefinitions {
+
+    @Given("que estoy en el servicio")
+    public void queEstoyEnElServicio() {
+
+    }
+
+    @When("y realizo una peticion")
+    public void yRealizoUnaPeticion() {
+
+    }
+
+    @Then("obtendre un status {int}")
+    public void obtendreUnStatus(Integer int1) {
+
+
+    }
+}
---
+++ src/test/resources/features/singleuser.feature
@@ -0,0 +1,9 @@
+Feature: consultar usuario
+  Como automatizador
+  Quiero validar que existe el usuario
+  Para validar existencia en plataforma
+
+  Scenario: consultar un usuario
+    Given que estoy en el servicio
+    When y realizo una peticion
+    Then obtendre un status 200
